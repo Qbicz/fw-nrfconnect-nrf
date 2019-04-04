ef627840b2c8a4df37d0b4d5f88ca364f6cb94eb
Filip Kubicz <filip.kubicz@nordicsemi.no>
2019-04-05 00:23:55 +0200

    knowledge sharing: state as look-up table
    Signed-off-by: Filip Kubicz <filip.kubicz@nordicsemi.no>

--- samples/patterns/state_transition_table/src/main.c
+++ samples/patterns/state_transition_table/src/main.c
@@ -12,68 +12,53 @@
 #define BUTTON_OPEN	0x01
 #define BUTTON_CLOSE	0x02
 
+#define BUTTON_COUNT	2
+#define EVENT_COUNT	BUTTON_COUNT
+#define EVENT_ID(x)	(x - 1)
+
 /* Car door lock state */
 enum door_state {
 	STATE_OPEN,
-	STATE_CLOSED
+	STATE_CLOSED,
+
+	STATE_COUNT
 };
 
 struct automobile {
 	enum door_state state;
 };
 
-struct automobile car;
+static struct automobile car;
+
+static enum door_state transition_table[STATE_COUNT][EVENT_COUNT] = {
+	{STATE_OPEN, STATE_CLOSED},
+	{STATE_OPEN, STATE_CLOSED}
+};
 
 void door_open(void) {
-	switch (car.state) {
-	case STATE_OPEN:
-		printk("ALREADY OPEN\n");
-		/* Do nothing */
-		break;
+	car.state = transition_table[car.state][EVENT_ID(BUTTON_OPEN)];
 
-	case STATE_CLOSED:
-		printk("OPEN THE CAR\n");
-		car.state = STATE_OPEN;
-		break;
-
-	default:
-		__ASSERT_NO_MSG(false);
-		break;
-	}
+	printk("Transition to %s", ((car.state == STATE_OPEN) ? "OPEN":"CLOSED"));
 }
 
 void door_close(void) {
-	switch (car.state) {
-	case STATE_OPEN:
-		printk("CLOSE THE CAR\n");
-		car.state = STATE_CLOSED;
-		break;
+	car.state = transition_table[car.state][EVENT_ID(BUTTON_CLOSE)];
 
-	case STATE_CLOSED:
-		printk("ALREADY CLOSED\n");
-		/* Do nothing */
-		break;
-
-	default:
-		__ASSERT_NO_MSG(false);
-		break;
-	}
+	printk("Transition to %s", ((car.state == STATE_OPEN) ? "OPEN":"CLOSED"));
 }
 
 void button_changed(u32_t button_state, u32_t has_changed)
 {
-	if (has_changed == BUTTON_OPEN && button_state & BUTTON_OPEN) {
+	if ((has_changed == BUTTON_OPEN) && (button_state & BUTTON_OPEN)) {
 		printk("user requested: open\n");
 		door_open();
 	}
 
-	if (has_changed == BUTTON_CLOSE && button_state & BUTTON_CLOSE) {
+	else if ((has_changed == BUTTON_CLOSE) && (button_state & BUTTON_CLOSE)) {
 		printk("user requested: close\n");
 		door_close();
 	}
 }
-
-
 
 void configure_buttons(void)
 {
@@ -86,7 +71,7 @@ void configure_buttons(void)
 
 void main(void)
 {
-	printk("Start zephyr\n");
+	printk("State: transition table\n");
 
 	car.state = STATE_CLOSED;
 	configure_buttons();
